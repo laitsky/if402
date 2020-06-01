4cec7ff4b0cadcf87d5fdd489db2dca4cab8c6d9
laitsky <vincent.diamond15@gmail.com>
2020-06-01 22:18:06 +0700

    uas done revisi

--- UAS_27047_VincentDiamond/src/umn/ac/id/_27047/Main.java
+++ UAS_27047_VincentDiamond/src/umn/ac/id/_27047/Main.java
@@ -4,86 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<MediaPromosi> mediaPromosis = new ArrayList<>();
     static ArrayList<Aktor> aktors = new ArrayList<>();
     static ArrayList<Celebgram> celebgrams = new ArrayList<>();
     static ArrayList<Majalah> majalahs = new ArrayList<>();
     static ArrayList<Baliho> balihos = new ArrayList<>();
     static ArrayList<Order> orders = new ArrayList<>();
-
-    private static void seedData() {
-        aktors.add(new Aktor("Chris Evans", 30, 170, 60, 100_000, "Captain America Lovers"));
-        aktors.add(new Aktor("Robert Downey Jr.", 31, 171, 61, 120_000, "Ironman Lovers"));
-        aktors.add(new Aktor("Chris Hemsworth", 32, 172, 62, 140_000, "Thor Lovers"));
-        celebgrams.add(new Celebgram("Awkarin", 25, 165, 45, 100_000, "Instagram", "Furnitur"));
-        celebgrams.add(new Celebgram("Anya Geraldine", 26, 166, 46, 120_000, "Twitter", "Makanan"));
-        celebgrams.add(new Celebgram("Devina Aurel", 27, 167, 47, 140_000, "Youtube", "Tanaman"));
-        balihos.add(new Baliho("Baliho Jakarta Pusat", 6, "Jakarta Pusat"));
-        balihos.add(new Baliho("Baliho Jakarta Barat", 8, "Jakarta Barat"));
-        balihos.add(new Baliho("Baliho Jakarta Utara", 9, "Jakarta Utara"));
-        majalahs.add(new Majalah("Home Decor", 15, "Halaman 2", "Furnitur"));
-        majalahs.add(new Majalah("Sedap", 18, "Halaman 3", "Makanan"));
-        majalahs.add(new Majalah("Otomotif", 21, "Halaman 4", "Tanaman"));
-    }
-
-    private static void menuUtama() {
-        System.out.println("===============================");
-        System.out.println("\tWemen Marketing Agent\t");
-        System.out.println("===============================");
-        System.out.println("1. Penambahan Media");
-        System.out.println("2. Penyewaan Media");
-        System.out.println("3. Check Employment / Rental");
-        System.out.println("4. Credits");
-        System.out.println("5. Exit");
-        System.out.println("===============================");
-        System.out.print("Pilih Menu (1-5): ");
-    }
-
-    private static void menuPenambahanMedia() {
-        System.out.println("===============================");
-        System.out.println("\tWemen Marketing Agent\t");
-        System.out.println("===============================");
-        System.out.println("1. Celebgram");
-        System.out.println("2. Majalah");
-
-        System.out.println("===============================");
-        System.out.print("Pilih Media (1-2): ");
-    }
-
-    private static void showKategori() {
-        System.out.println("===============================");
-        System.out.println("1. Furnitur");
-        System.out.println("2. Makanan");
-        System.out.println("3. Tanaman");
-        System.out.println("4. Otomotif");
-        System.out.println("5. Musik");
-        System.out.println("===============================");
-        System.out.print("Pilih Kategori (1-5): ");
-    }
-
-    private static void printOrders() {
-        int idx = 1;
-        System.out.println("===============================");
-        System.out.println("\tWemen Marketing Agent\t");
-        System.out.println("===============================");
-        for (Order o : orders) {
-            System.out.println("No\t\t\t: " + (idx));
-            System.out.println("Media\t\t: " + o.getMedia());
-            System.out.println("Nama\t\t: " + o.getMediaPromosi().getNama());
-            System.out.println("Tanggal\t\t: " + o.getTanggal());
-            System.out.println("Durasi Kontrak: " + o.getJangkaWaktu());
-            System.out.println("Harga\t\t: " + o.getMediaPromosi().getHarga());
-        }
-    }
-
-    private static void showCredit() {
-        System.out.println("===============================");
-        System.out.println("\tWemen Marketing Agent\t");
-        System.out.println("===============================");
-        System.out.println("Name: Vincent Diamond");
-        System.out.println("NIM: 00000027047");
-        System.out.println("===============================");
-    }
 
     public static void main(String[] args) {
         seedData();
@@ -173,6 +98,7 @@ public class Main {
                     int idx = 1;
                     int reqIdx;
                     int total = 0;
+                    int totalPayment;
                     int nominalPembayaran;
                     int aktorIdx = aktors.size();
                     int celebIdx = aktorIdx + celebgrams.size();
@@ -216,7 +142,7 @@ public class Main {
                         System.out.println("Berat Badan\t: " + a.getBeratBadan());
                         System.out.println("Jumlah Fans\t: " + a.getFans());
                         System.out.println("Nama Fansclub: " + a.getFanClub());
-                        System.out.println("Harga\t\t: " + a.getHarga());
+                        System.out.println("Harga\t\t: " + (a.getHarga() * jmlHariSewa));
                         idx++;
                     }
                     for (Celebgram c : celebgrams) {
@@ -231,9 +157,9 @@ public class Main {
                         System.out.println("Bidang\t\t: " + c.getBidang());
                         System.out.print("Harga\t\t: ");
                         if (c.getBidang().equals(bidang)) {
-                            System.out.println(c.getHarga() + (50_000 * jmlHariSewa));
+                            System.out.println(c.getHarga() + (c.getHarga() * jmlHariSewa + 50_000 * jmlHariSewa));
                         } else {
-                            System.out.println(c.getHarga());
+                            System.out.println(c.getHarga() * jmlHariSewa);
                         }
                         idx++;
                     }
@@ -244,7 +170,7 @@ public class Main {
                         System.out.println("Nama\t\t: " + b.getNama());
                         System.out.println("Luas\t\t: " + b.getLuas());
                         System.out.println("Letak\t\t: " + b.getLetak());
-                        System.out.println("Harga\t\t: " + b.getHarga());
+                        System.out.println("Harga\t\t: " + b.getHarga() * jmlHariSewa);
                         idx++;
                     }
                     for (Majalah m : majalahs) {
@@ -257,9 +183,9 @@ public class Main {
                         System.out.println("Bidang\t\t: " + m.getBidang());
                         System.out.print("Harga\t\t: ");
                         if (m.getBidang().equals(bidang)) {
-                            System.out.println(m.getHarga() + (50_000 * jmlHariSewa));
+                            System.out.println(m.getHarga() + (m.getHarga() * jmlHariSewa + 200_000 * jmlHariSewa));
                         } else {
-                            System.out.println(m.getHarga());
+                            System.out.println(m.getHarga() * jmlHariSewa);
                         }
                         idx++;
                     }
@@ -272,7 +198,8 @@ public class Main {
                         if (nominalPembayaran < aktors.get(reqIdx).getHarga() * jmlHariSewa) {
                             System.out.println("Uang tidak mencukupi");
                         } else {
-                            orders.add(new Order(aktors.get(reqIdx - 1), bidang, tglMulai, "Aktor", jmlHariSewa, total));
+                            totalPayment = aktors.get(reqIdx).getHarga() * jmlHariSewa;
+                            orders.add(new Order(aktors.get(reqIdx - 1), bidang, tglMulai, "Aktor", jmlHariSewa, totalPayment));
                             System.out.println("Pemesanan anda sukses");
                         }
                     } else if (reqIdx <= celebIdx) {
@@ -283,7 +210,8 @@ public class Main {
                         if (nominalPembayaran < celebgrams.get(reqIdx).getHarga() * jmlHariSewa + total) {
                             System.out.println("Uang tidak mencukupi");
                         } else {
-                            orders.add(new Order(celebgrams.get(reqIdx), bidang, tglMulai, "Celebgram", jmlHariSewa, total));
+                            totalPayment = (celebgrams.get(reqIdx).getHarga() * jmlHariSewa) + total;
+                            orders.add(new Order(celebgrams.get(reqIdx), bidang, tglMulai, "Celebgram", jmlHariSewa, totalPayment));
                             System.out.println("Pemesanan anda sukses");
                         }
                     } else if (reqIdx <= balihoIdx) {
@@ -291,6 +219,7 @@ public class Main {
                         if (nominalPembayaran < balihos.get(reqIdx).getHarga() * jmlHariSewa) {
                             System.out.println("Uang tidak mencukupi");
                         } else {
+                            totalPayment = balihos.get(reqIdx).getHarga() * jmlHariSewa;
                             orders.add(new Order(balihos.get(reqIdx), bidang, tglMulai, "Baliho", jmlHariSewa, total));
                             System.out.println("Pemesanan anda sukses");
                         }
@@ -302,7 +231,8 @@ public class Main {
                         if (nominalPembayaran < majalahs.get(reqIdx).getHarga() * jmlHariSewa + total) {
                             System.out.println("Uang tidak mencukupi");
                         } else {
-                            orders.add(new Order(majalahs.get(reqIdx), bidang, tglMulai, "Majalah", jmlHariSewa, total));
+                            totalPayment = (majalahs.get(reqIdx).getHarga() * jmlHariSewa) + total;
+                            orders.add(new Order(majalahs.get(reqIdx), bidang, tglMulai, "Majalah", jmlHariSewa, totalPayment));
                             System.out.println("Pemesanan anda sukses");
                         }
                     } else {
@@ -346,5 +276,79 @@ public class Main {
                     System.out.println("Angka yang dimasukkan tidak valid!");
             }
         }
+    }
+
+    private static void seedData() {
+        aktors.add(new Aktor("Chris Evans", 30, 170, 60, 100_000, "Captain America Lovers"));
+        aktors.add(new Aktor("Robert Downey Jr.", 31, 171, 61, 120_000, "Ironman Lovers"));
+        aktors.add(new Aktor("Chris Hemsworth", 32, 172, 62, 140_000, "Thor Lovers"));
+        celebgrams.add(new Celebgram("Awkarin", 25, 165, 45, 100_000, "Instagram", "Furnitur"));
+        celebgrams.add(new Celebgram("Anya Geraldine", 26, 166, 46, 120_000, "Twitter", "Makanan"));
+        celebgrams.add(new Celebgram("Devina Aurel", 27, 167, 47, 140_000, "Youtube", "Tanaman"));
+        balihos.add(new Baliho("Baliho Jakarta Pusat", 6, "Jakarta Pusat"));
+        balihos.add(new Baliho("Baliho Jakarta Barat", 8, "Jakarta Barat"));
+        balihos.add(new Baliho("Baliho Jakarta Utara", 9, "Jakarta Utara"));
+        majalahs.add(new Majalah("Home Decor", 15, "Halaman 2", "Furnitur"));
+        majalahs.add(new Majalah("Sedap", 18, "Halaman 3", "Makanan"));
+        majalahs.add(new Majalah("Otomotif", 21, "Halaman 4", "Tanaman"));
+    }
+
+    private static void menuUtama() {
+        System.out.println("===============================");
+        System.out.println("\tWemen Marketing Agent\t");
+        System.out.println("===============================");
+        System.out.println("1. Penambahan Media");
+        System.out.println("2. Penyewaan Media");
+        System.out.println("3. Check Employment / Rental");
+        System.out.println("4. Credits");
+        System.out.println("5. Exit");
+        System.out.println("===============================");
+        System.out.print("Pilih Menu (1-5): ");
+    }
+
+    private static void menuPenambahanMedia() {
+        System.out.println("===============================");
+        System.out.println("\tWemen Marketing Agent\t");
+        System.out.println("===============================");
+        System.out.println("1. Celebgram");
+        System.out.println("2. Majalah");
+
+        System.out.println("===============================");
+        System.out.print("Pilih Media (1-2): ");
+    }
+
+    private static void showKategori() {
+        System.out.println("===============================");
+        System.out.println("1. Furnitur");
+        System.out.println("2. Makanan");
+        System.out.println("3. Tanaman");
+        System.out.println("4. Otomotif");
+        System.out.println("5. Musik");
+        System.out.println("===============================");
+        System.out.print("Pilih Kategori (1-5): ");
+    }
+
+    private static void printOrders() {
+        int idx = 1;
+        System.out.println("===============================");
+        System.out.println("\tWemen Marketing Agent\t");
+        System.out.println("===============================");
+        for (Order o : orders) {
+            System.out.println("No\t\t\t: " + (idx));
+            System.out.println("Media\t\t: " + o.getMedia());
+            System.out.println("Nama\t\t: " + o.getMediaPromosi().getNama());
+            System.out.println("Tanggal\t\t: " + o.getTanggal());
+            System.out.println("Durasi Kontrak: " + o.getJangkaWaktu());
+            System.out.println("Harga\t\t: " + o.getMediaPromosi().getHarga());
+        }
+    }
+
+    private static void showCredit() {
+        System.out.println("===============================");
+        System.out.println("\tWemen Marketing Agent\t");
+        System.out.println("===============================");
+        System.out.println("Name: Vincent Diamond");
+        System.out.println("NIM: 00000027047");
+        System.out.println("===============================");
     }
 }
